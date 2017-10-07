afaf31806b827336d25bd49cbbe974def300555a
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-10-07 12:03:57 +0100

    added pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
    before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 end
+
+class ApplicationController < ActionController::Base
+  include Pundit
+  protect_from_forgery
+end
+
+
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :email) }
    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
